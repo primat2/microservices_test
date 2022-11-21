a819306e937bfba42eb922d159b926480d3fcf01
artem.gavshin <artem.gavshin@ural.rt.ru>
2022-11-21 12:52:20 +0500

    Запуск сервера

--- MicroservicesTestJava/src/main/java/org/example/App.java
+++ MicroservicesTestJava/src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080).addService(new GreetingServiceImplementation()).build();
+        server.start();
+        System.out.println("SERVER STARTED");
+        server.awaitTermination();
     }
 }
